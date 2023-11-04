dc1744b6bd6cf1b8056525ba2b92e64e2423406a
Dmitry Parkhomenko <oku_74@mail.ru>
2023-11-04 22:50:54 +0500

    My traffic light finished.

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redColor: UIView!
+    @IBOutlet weak var yellowColor: UIView!
+    @IBOutlet weak var greenColor: UIView!
+    @IBOutlet weak var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+        redColor.layer.cornerRadius = 69
+        redColor.alpha = 0.2
+        
+        yellowColor.layer.cornerRadius = 69
+        yellowColor.alpha = 0.2
+        
+        greenColor.layer.cornerRadius = 69
+        greenColor.alpha = 0.2
+        
+        button.layer.cornerRadius = 20
     }
-
-
+      
+    @IBAction func buttonGetTapped(_ sender: UIButton) {
+        sender.setTitle("NEXT", for: .normal)
+        if redColor.alpha == 1 {
+            redColor.alpha = 0.2
+            yellowColor.alpha = 1
+        } else if yellowColor.alpha == 1 {
+            yellowColor.alpha = 0.2
+            greenColor.alpha = 1
+        } else if greenColor.alpha == 1 {
+            greenColor.alpha = 0.2
+            redColor.alpha = 1
+        } else {
+            redColor.alpha = 1
+        }
+    }
 }
 
